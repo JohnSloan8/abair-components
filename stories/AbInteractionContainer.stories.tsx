f36bd10dace57514bda2974e8fc771e99583af42
john <johnsloan1984@gmail.com>
2022-12-06 12:07:08 +0000

    done interactioncontainer

--- stories/AbInteractionContainer.stories.tsx
+++ stories/AbInteractionContainer.stories.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
+import { AbTextField } from '../src';
 import {
-  AbRecognitionWaveVisual,
-  AbRecognitionWaveVisualProps,
-} from '../src/AbRecognitionWaveVisual';
+  AbInteractionContainer,
+  AbInteractionContainerProps,
+} from '../src/AbInteractionContainer';
 
 const meta: Meta = {
-  title: 'AbRecognitionWaveVisual',
-  component: AbRecognitionWaveVisual,
+  title: 'AbInteractionContainer',
+  component: AbInteractionContainer,
 };
 
-const Template: Story<AbRecognitionWaveVisualProps> = args => (
-  <AbRecognitionWaveVisual {...args} />
+const Template: Story<AbInteractionContainerProps> = args => (
+  <AbInteractionContainer {...args} />
 );
 
 const Default = Template.bind({});
+Default.args = {
+  textbox: <AbTextField />,
+  color: 'warning.light',
+};
+
+const Synthesis = Template.bind({});
+Synthesis.args = {
+  textbox: <AbTextField />,
+  color: 'secondary.light',
+};
 
 export default meta;
-export { Default };
+export { Default, Synthesis };
